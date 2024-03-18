62c52e9acac55258e999f10da71a4bd122ae620f
Alfred Wallace <contact@alfredwallace.me>
2024-03-17 22:51:21 -0700

    Added basic fetch logic, haven't tested.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    function PerformFetch(cityName) {
+        fullURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5";
+
+        fetch(fullURL, {
+            method: "GET",
+            credentials: "same-origin",
+            redirect: "follow", 
+        })
+            .then(function (response) {
+                console.log("response: ");
+                console.log(response);
+                return response.json();
+            })
+            .then(function (data) {
+                console.log("data: ");
+                console.log(data);
+            });
+
+    }
+
     // Search button event listener
-    $("#searchButton").on("click", function(){
+    $("#searchButton").on("click", function () {
         var searchInput = $("input#citySearchInput").val().trim();
         console.log(searchInput);
         if (searchInput == "") {
@@ -10,18 +30,26 @@ $(document).ready(function(){
         // TODO: perform FETCH
 
     });
-    // TODO: Clear the previous searches defaults
     
+    // TODO: Clear the previous searches defaults
+    var savedSearches = localStorage.getItem("savedSearches");
+    console.log("savedSearches: ");
+    console.log(savedSearches);
+    // If no input, set it to an empty array
+    if (savedSearches == null) {
+        localStorage.setItem("savedSearches", []);
+    }
+
     // TODO: Check for previous searches (localStorage)
 
     // TODO: If there are results, populate the list
 
     // TODO: Previous Search button event listener
-    $("button.previousSearchInput").click(function(){
+    $("button.previousSearchInput").click(function () {
         // TODO: perform FETCH
 
     });
 
-    
+
 
 });
